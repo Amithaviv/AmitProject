df1652a5aab8c83ece7f9d9a58b18dfc5a3bd553
Amithaviv <amithaviv24@gmail.com>
2022-09-06 11:02:53 +0300

    fix DAL api & SingleProduct component

--- src/DAL/serverFunctions.js
+++ src/DAL/serverFunctions.js
@@ -1,10 +1,9 @@
-  export async function getUsers() {
-    let customers = await fetch("http://localhost:3100/customers/session",{credentials:"include"});
-    customers = await customers.json();
-    console.log(customers);
-  }
-  getUsers()
 
+  export function getUsers() {
+    return fetch("http://localhost:3100/customers/session",{credentials:"include"})
+        .then(response => response.json())
+        .then(customers => (customers))
+}
 
   export function getProducts() {
     return fetch("http://localhost:3100/products/session",{credentials:"include"})
